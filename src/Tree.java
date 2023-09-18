e85a017f5f740f6539fe31010304a7823d043159
mottunad <nadim.mottu@mail.utoronto.ca>
2023-09-18 18:49:40 -0400

    added multiset adt

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,46 @@
-public class Tree {
+import java.util.Arrays;
+import java.util.List;
+
+public class Tree<T>{
+    Tree(T root, List<Tree> subtrees) {
+
+    }
+    Tree(List<Tree> subtrees) {
+
+    }
+    public boolean is_empty() {
+
+        return false;
+    }
+    public int len() {
+
+        return 0;
+    }
+    public int count(T item) {
+
+        return 0;
+    }
+    public String toString() {
+
+        return "";
+    }
+    private String _str_indented(int depth) {
+
+        return "";
+    }
+    public String _str_indented() {
+
+        return "";
+    }
+    public float average() { //check if I should switch to double
+
+        return 0.0F;
+    }
+
+    private Arrays _average_helper() {
+
+
+        return null;
+    }
 
 }
