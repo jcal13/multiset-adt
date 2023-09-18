bd6d2726a0c2f261450be99faeb2495bdfedf426
Concordski-laptop <franklin.wu.1231@gmail.com>
2023-09-18 16:51:31 -0400

    something

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,3 @@
-import java.util;
 import java.util.ArrayList;
 
 public class Tree {
@@ -8,5 +7,18 @@ public class Tree {
     Tree(Object root, ArrayList<Tree> subtrees) {
         this.root = root;
         this.subtrees = subtrees;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public int length() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+        else {
+            return 1; // this is not right
+        }
     }
 }
