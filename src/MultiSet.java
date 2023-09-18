e032016b0cfd7743dc9bebfb8004a18c62e1a5f1
Maria Ma <maria.shurui.ma@gmail.com>
2023-09-18 18:53:08 -0400

    implemented MultiSet interface

---
+++ src/MultiSet.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.time.*;
+
+public interface MultiSet<T> {
+
+    public boolean add(T item);
+
+    public void remove(T item);
+
+    public boolean contains(T item);
+
+    public boolean is_empty();
+
+    public int count(T item);
+
+    public int size();
+
+}
