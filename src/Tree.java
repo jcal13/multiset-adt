e7c1abbb028dbc810357f2d29487176219750bb0
Dorothy Lee <dot.lee@mail.utoronto.ca>
2023-09-18 18:42:18 -0400

    Tree init finished

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,21 @@
-public class Tree {
+import java.util.*;
+public class Tree<T> {
+    private T root;
+    private List<Tree> subtrees;
+
+    public Tree(T root) {
+        this(root, null);
+    }
+
+    public Tree(T root, List<Tree> subtrees) {
+        this.root = root;
+        if (root == null) {
+            this.subtrees = null;
+        }
+        else {
+            this.subtrees = subtrees;
+        }
+    }
+
+    public Tree len()
 }
