103a4dbb99f243f42022a06ea8fbdbceb8898ef5
Yakkuu <jack2016zhang@gmail.com>
2023-09-18 18:59:58 -0400

    tried converting Tree Python code to Java

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Tree {
+
+    private Object root;
+    private ArrayList<Tree> subtrees;
+
+
+    public Tree(Object root, Object subtrees) {
+    this.root = root;
+    if (subtrees == null) {
+        this.subtrees = new ArrayList<Tree>();
+    }
+    else {
+        this.subtrees = new ArrayList<Tree>(subtrees); // idk how to do this aaaa
+    }
+    }
+
+public boolean is_empty() {
+    return false;
+}
+
 }
