26eff463abc346c1525462bd3411194b41a01161
Concordski-laptop <franklin.wu.1231@gmail.com>
2023-09-18 16:36:27 -0400

    constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,12 @@
+import java.util;
+import java.util.ArrayList;
+
 public class Tree {
+    private Object root;
+    private ArrayList<Tree> subtrees;
+
+    Tree(Object root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
 }
