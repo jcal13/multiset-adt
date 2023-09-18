c0a593d3169eea832c520785b57ef69151ee26f2
zhizhongchen <markson.chen@mail.utoronto.ca>
2023-09-18 17:29:06 -0400

    Created Java MultiSet Interface & Let Tree implemented MultiSet

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,33 @@
-public class Tree {
+public class Tree<T> implements MultiSet<T>{
+
+
+    @Override
+    public boolean add(T item) {
+        return false;
+    }
+
+    @Override
+    public void remove(T item) {
+
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return false;
+    }
+
+    @Override
+    public boolean is_empty() {
+        return false;
+    }
+
+    @Override
+    public int count(T item) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
