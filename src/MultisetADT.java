42c3c6d9542536cf98953458eba6e155fab77007
Prashanth Reddy Shyamala <prashanth.shyamala@mail.utoronto.ca>
2023-09-18 16:37:33 -0400

    added the multiset abstract class

---
+++ src/MultisetADT.java
@@ -0,0 +1,9 @@
+public interface MultisetADT {
+    public boolean add(Object item);
+    public void remove(Object item);
+    public boolean contains(Object item);
+    public boolean contains();
+    public int count(Object item);
+    public int size();
+
+}
