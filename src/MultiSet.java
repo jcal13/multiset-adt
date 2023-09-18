92c3ff47d9202a973bcdebd6497612f8df687245
Franco Valencia <francomiguel.valencia@gmail.com>
2023-09-18 12:50:03 -0400

    completed abstract multiset class

---
+++ src/MultiSet.java
@@ -0,0 +1,15 @@
+public abstract class MultiSet<T> {
+    //    An abstract class representing the MultiSet ADT, which supports the
+    //    add, remove, is_empty, count, and contains operations.
+    //
+    //    This class itself does not handle how the underlying data is stored,
+    //    so it just inherits Object.__init__.
+
+    public abstract boolean add(T item);
+    public abstract void remove(T item);
+    public abstract boolean contains(T item);
+    public abstract boolean isEmpty (T item);
+    public abstract Integer count(T item);
+    public abstract Integer size(T item);
+
+}
