7367542143a4496a5c6d03b5851ee3babf938de9
Dharma <dharma.ong@mail.utoronto.ca>
2023-09-18 16:55:01 -0400

    finished constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,48 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 
-public class Tree {
+public class Tree implements MultisetADT {
     private int root;
     private ArrayList<Tree> subtrees;
 
+    public Tree(int root, ArrayList<Tree> subtrees) {
+        this.root = root;
+
+        if (subtrees.isEmpty()) {
+            this.subtrees = null;
+        }
+        else {
+            this.subtrees = subtrees;
+        }
+    }
 
 
+    @Override
+    public boolean add(Object item) {
+        return false;
+    }
+
+    @Override
+    public void remove(Object item) {
+
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean contains() {
+        return false;
+    }
+
+    @Override
+    public int count(Object item) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
