8db4afbdef8037b787e281678eb3a91f1a5aec14
Christoffer Tan <tanchristoffer@gmail.com>
2023-09-18 18:34:58 -0400

    Test 1

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,9 @@
 public class Tree {
+    private Object[] root;
+    private Tree[] subtrees;
+
+    Tree(Object[] root, Tree[] subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
 }
