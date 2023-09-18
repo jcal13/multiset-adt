5f817c3e03fc669fa7d8f107c8d87ac526bd149b
Alicia <Alicia@az-macbook-air.local>
2023-09-18 10:39:57 -0400

    added to Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,13 @@
 public class Tree {
+    private Integer root;
+    private Tree[] subtrees;
+
+    public Tree (Integer root, Tree[] subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    public String printRoot() {
+        return root.toString();
+    }
 }
