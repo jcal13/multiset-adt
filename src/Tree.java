0274741e72f62a4bcfcf43b813d814757644fa16
Ibrahim Shanqiti <ibrahim.shanqiti@gmail.com>
2023-09-18 20:34:53 -0400

    Adding the first part of Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,11 @@
+import java.util.*;
 public class Tree {
+    private int root;
+    private List<Tree> subtrees;
+
+    public Tree(int root, List<Tree> subtrees){
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
 }
