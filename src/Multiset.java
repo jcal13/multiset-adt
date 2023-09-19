c647af2c31f6fc5d7120045fd1dbc3186f586956
Akunna <akunna.megwa@gmail.com>
2023-09-19 17:23:37 -0400

    akunna worked on the multiset class

--- src/Multiset.java
+++ src/Multiset.java
@@ -1,2 +1,32 @@
 public class Multiset {
+    public boolean add(Object item) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    public void remove(Object item) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    public boolean contains(Object item) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    public boolean is_empty() {
+        throw new UnsupportedOperationException();
+
+    }
+
+    public int count(Object item) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    public int size() {
+
+        throw new UnsupportedOperationException();
+    }
+
 }
